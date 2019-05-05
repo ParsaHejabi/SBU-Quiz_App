10078b8dd3a917ce0003b306d8279d08b66e29c3
Parsa Hejabi <parsa.hejabi@gmail.com>
2019-05-06 04:01:22 +0430

    Merged 2 repos.

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, {Component} from "react";
+import {Route, NavLink, HashRouter} from "react-router-dom";
+import "./App.css";
+import Upload from "./upload/Upload";
+import Questions from "./questions/Questions";
+
+class App extends Component {
+    render() {
+        return (
+            <HashRouter>
+                <div>
+                    <ul className="header">
+                        <li><NavLink to="/">Questions</NavLink></li>
+                        <li><NavLink to="/upload">Upload</NavLink></li>
+                    </ul>
+                    <div className="App">
+                        <div className="Card">
+                            <Route exact path="/" component={Questions}/>
+                            <Route path="/upload" component={Upload}/>
+                        </div>
+                    </div>
+                </div>
+            </HashRouter>
+        );
+    }
+}
+
+export default App;
